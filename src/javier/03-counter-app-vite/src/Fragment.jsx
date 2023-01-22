276b34f84cec9e4095113cab6febdd2be7689788
Javier López Malone <malonejv@gmail.com>
2023-01-22 18:56:36 +0100

    Fragment

---
+++ src/javier/03-counter-app-vite/src/Fragment.jsx
@@ -0,0 +1,37 @@
+import { Fragment } from "react";
+
+export const MyComponentWithoutFramgment1 = ()=>{
+    return(
+        <h1>Un elemento por componente</h1>
+    );
+}
+
+
+export const MyComponentWithoutFramgment2 = ()=>{
+    return(
+        <div>
+            <h1>Dos elementos</h1>
+            <h2>Pero requerimos de un padre</h2>
+        </div>
+    );
+}
+
+export const MyFragment1 = ()=>{
+    return (
+        <Fragment>
+            <h1>Dos elementos</h1>
+            <h2>Pero requerimos de un padre</h2>
+        </Fragment>
+    )
+}
+
+//Esto también es un fragmento pero no requiere importar Fragment
+export const MyFragment2 = ()=>{
+    return (
+        <>
+            <h1>Dos elementos</h1>
+            <h2>Pero requerimos de un padre</h2>
+            <h3>Sin importar Fragment</h3>
+        </>
+    )
+}
--- src/javier/03-counter-app-vite/src/main.jsx
+++ src/javier/03-counter-app-vite/src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { MyApp } from "./MyApp";
+import {MyComponentWithoutFramgment1, MyComponentWithoutFramgment2, MyFragment } from "./Fragment";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <MyApp />
+        <MyComponentWithoutFramgment1 />
+        <MyComponentWithoutFramgment2 />
+        <MyFragment />
     </React.StrictMode>
 )
