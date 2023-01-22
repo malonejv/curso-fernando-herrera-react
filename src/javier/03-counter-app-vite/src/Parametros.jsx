9d2f1b22d2dec8b197b6c9554bb1c5173f81facf
Javier López Malone <malonejv@gmail.com>
2023-01-22 19:05:24 +0100

    Uso variables dentro de mi Functional component

---
+++ src/javier/03-counter-app-vite/src/Parametros.jsx
@@ -0,0 +1,10 @@
+//Esto también es un fragmento pero no requiere importar Fragment
+const newMessage = 'Test Parameter';
+
+export const Parametros = ()=>{
+    return (
+        <>
+            <h1>Parametro 1: { newMessage }</h1>
+        </>
+    )
+}
--- src/javier/03-counter-app-vite/src/main.jsx
+++ src/javier/03-counter-app-vite/src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { MyApp } from "./MyApp";
-import {MyComponentWithoutFramgment1, MyComponentWithoutFramgment2, MyFragment } from "./Fragment";
+import {MyComponentWithoutFramgment1, MyComponentWithoutFramgment2, MyFragment1, MyFragment2 } from "./Fragment";
+import { Parametros } from "./Parametros";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <MyApp />
         <MyComponentWithoutFramgment1 />
         <MyComponentWithoutFramgment2 />
-        <MyFragment />
+        <MyFragment1 />
+        <MyFragment2 />
+        <Parametros></Parametros>
     </React.StrictMode>
 )
